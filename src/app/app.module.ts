8c6966bbc815ddf9b38a536133c78c1f4f7bcae7
Naomichi Kuroda <naomichi.kuroda.jp@gmail.com>
2016-12-24 13:33:35 +0900

    Add RoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
+// Module
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from "./app-routing.module";
 
+// Service
+
+// Page
+
+// Component
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
